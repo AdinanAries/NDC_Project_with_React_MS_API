9120c131ba1672915c8b524890c1416dabadd6bd
Mohammed Adinan <adinanaries@outlook.com>
2020-10-25 11:42:47 -0400

    added component for displaying Post updates User infor for user who posted the update

---
+++ front-end/react-ui/src/components/page_cards_cards/post_card_helpers/post_update_poster_infor.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+
+class PostUpdatePosterInfor extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+
+        }
+    }
+
+    reder(){
+        return{
+            
+        }
+    }
+}
