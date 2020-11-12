7126cce4db57d1098179c7fd055c644b02bd9220
Mohammed Adinan <adinanaries@outlook.com>
2020-11-11 21:21:33 -0500

    changed font size for top ads link

--- front-end/react-ui/src/components/body/main.js
+++ front-end/react-ui/src/components/body/main.js
@@ -8,7 +8,7 @@ class Main extends React.Component{
         return (
             <main>
                 <div class="wrapper">
-                <h1 style={{paddingBottom: "20px", width: "fit-content", margin: "auto", fontSize: "16px", color: "#667676"}}>
+                <h1 style={{paddingBottom: "20px", paddingTop: "5px", width: "fit-content", margin: "auto", fontSize: "15px", color: "#667676"}}>
                             <span style={{fontWeight: "bolder", color: "#0e3c99", fontSize: "16px"}}>When is the next election? </span>
                             We are here to make sure you get all the important national notifications.
                             <i style={{marginLeft: 15}} className="fa fa-ellipsis-h"></i>
